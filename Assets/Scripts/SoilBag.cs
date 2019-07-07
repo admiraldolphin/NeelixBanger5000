3fb628d7d33aec4ba697e62b16b8c03713cb315b
Jon Manning <jon@secretlab.com.au>
2019-07-07 15:52:53 +1000

    Add slots; add darkfridge and fridge setup; add bag

--- Assets/Scripts/SoilBag.cs
+++ Assets/Scripts/SoilBag.cs
@@ -6,6 +6,51 @@ public class SoilBag : MonoBehaviour
 {
     public Soil soil = Soil.dirt;
 
+    public Transform stickyNotePosition;
+
+    public GameObject[] stickyNoteModels;
+
+    public Material stickyNoteBoneChar;
+    public Material stickyNoteTar;
+    public Material stickyNoteAsh;
+    public Material stickyNoteSand;
+    public Material stickyNoteDirt;
+
+    private void Awake() {
+        
+        GameObject noteModel = stickyNoteModels[Random.Range(0, stickyNoteModels.Length)];
+
+        Material noteMaterial = null;
+
+        switch (soil) {
+            case Soil.ash:
+            noteMaterial = stickyNoteAsh;
+            break;
+            case Soil.bonechar:
+            noteMaterial = stickyNoteBoneChar;
+            break;
+            case Soil.dirt:
+            noteMaterial = stickyNoteDirt;
+            break;
+            case Soil.sand:
+            noteMaterial = stickyNoteSand;
+            break;
+            case Soil.tar:
+            noteMaterial = stickyNoteTar;
+            break;
+        }
+
+        
+        var note = Instantiate(noteModel);
+
+        note.transform.SetParent(stickyNotePosition, true);
+        note.transform.position = stickyNotePosition.position;
+        note.transform.rotation = stickyNotePosition.rotation;
+
+        note.GetComponent<MeshRenderer>().material = noteMaterial;
+
+    }
+
     private Soil[] soils = (Soil[])System.Enum.GetValues(typeof(Soil));
     public int contents
     {
